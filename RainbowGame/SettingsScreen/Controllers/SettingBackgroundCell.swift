fa505fce9438853cf64d6b72979aa98d86c46386
Natalia <nata22808@mail.ru>
2023-11-14 19:47:47 +0300

    addSettingsScreen

---
+++ RainbowGame/SettingsScreen/Controllers/SettingBackgroundCell.swift
@@ -0,0 +1,8 @@
+//
+//  SettingBackgroundCell.swift
+//  RainbowGame
+//
+//  Created by Наталья Миронова on 14.11.2023.
+//
+
+import Foundation
---
+++ RainbowGame/SettingsScreen/Controllers/SettingTimeCell.swift
@@ -0,0 +1,8 @@
+//
+//  SettingTimeCell.swift
+//  RainbowGame
+//
+//  Created by Наталья Миронова on 14.11.2023.
+//
+
+import Foundation
