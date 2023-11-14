21730fa54f091063bdd419689874b2a3f4ce0326
Natalia <nata22808@mail.ru>
2023-11-14 21:03:58 +0300

    add Setting

--- RainbowGame/SettingsScreen/Controllers/SettingBackgroundCell.swift
+++ RainbowGame/SettingsScreen/Controllers/SettingBackgroundCell.swift
@@ -5,4 +5,55 @@
 //  Created by Наталья Миронова on 14.11.2023.
 //
 
-import Foundation
+import UIKit
+
+final class SettingBackgroundCell: UICollectionViewCell {
+	
+	static let id = "settingBackgroundCell"
+	
+	private let titleLabel = UILabel()
+	private let switchControl = UISwitch()
+	
+	private let stack = UIStackView()
+	
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		setupUI()
+		backgroundColor = .lightGray
+	}
+	
+	required init?(coder: NSCoder) {
+		super.init(coder: coder)
+	}
+	
+	private func setupUI() {
+		
+		addSubview(stack)
+		
+		[titleLabel, switchControl].forEach { stack.addArrangedSubview($0)}
+		
+		stack.axis = .horizontal
+		stack.spacing = 16
+		stack.distribution = .fill
+		stack.alignment = .center
+		
+		stack.translatesAutoresizingMaskIntoConstraints = false
+		
+		NSLayoutConstraint.activate([
+			stack.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+			stack.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
+			stack.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
+			stack.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -8)
+		])
+		
+		switchControl.addTarget(self, action: #selector(switchValueChanged), for: .valueChanged)
+	}
+	
+	func configure(with title: String) {
+		titleLabel.text = title
+	}
+	
+	@objc private func switchValueChanged() {
+		//print("Switch value changed to \(switchControl.isOn)")
+	}
+}
